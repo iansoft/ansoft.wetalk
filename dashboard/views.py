cc5bb8f7fb05f55e70ff401d16c1e37fbf8ba383
iansoft <ansoftware@163.com>
2015-06-17 21:58:26 +0800

    add the static file into django project

---
+++ dashboard/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse,JsonResponse, HttpResponseNotFound
+from django.template import RequestContext, loader
+import os
+
+def index(request):
+    template = loader.get_template('dashboard/index.html')
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+    static_path = os.path.join(base_dir, 'static')
+    context = RequestContext(request, {"base_dir": base_dir
+                                      ,"project_dir": PROJECT_PATH
+                                      ,"static_dir": static_path})
+    return HttpResponse(template.render(context))
